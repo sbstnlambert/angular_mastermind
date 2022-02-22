c68ff31625a0b2eec36b2a95b80044ae53321feb
sbstnlambert <sbstn.lambert@gmail.com>
2022-02-22 14:33:55 +0100

    Add secret code to main app

--- src/app/secret-code/secret-code.component.ts
+++ src/app/secret-code/secret-code.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-secret-code',
+  selector: 'secret-code',
   templateUrl: './secret-code.component.html',
   styleUrls: ['./secret-code.component.scss']
 })
@@ -10,7 +10,7 @@ export class SecretCodeComponent implements OnInit {
   secretCodes: string[] = [
     'blue',
     'red',
-    'gold',
+    'yellow',
     'green'
   ];
 
